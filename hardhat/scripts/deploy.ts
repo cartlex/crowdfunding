33c21f4cbf362b11241dfe9fc5032ed0614bf9e4
cartlex <ap26booking@gmail.com>
2023-03-09 08:10:07 +0300

    add deploy script

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,7 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
+    const [deployer] = await ethers.getSigners();
 
+    console.log(`Deploying contract with the account ${deployer.address}`)
+
+    const Factory = await ethers.getContractFactory("Crowdfunding");
+    const Crowdfunding = await Factory.deploy();
+    await Crowdfunding.deployed();
+    console.log(`Contract successfully deplouyed with address ${Crowdfunding.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
